f286ac7fc0ae12e73553fb12c3f76873da36d573
l3ra <lera2mail@gmail.com>
2023-12-22 09:11:06 +0000

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && 
+sudo apt-get upgrade &&
+mkdir vgg56 && 
+cd vgg56 &&
+wget https://raw.githubusercontent.com/JGuillaumin/SuperResGAN-keras/master/bsd100_callback.py &&
+wget https://raw.githubusercontent.com/JGuillaumin/SuperResGAN-keras/master/batch_generator.py &&
+wget https://raw.githubusercontent.com/JGuillaumin/SuperResGAN-keras/master/utils.py 
