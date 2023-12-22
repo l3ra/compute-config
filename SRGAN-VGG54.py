32d4b76ae2ab2122d0bea86db969587332722caa
l3ra <lera2mail@gmail.com>
2023-12-22 09:19:57 +0000

    Update SRGAN-VGG54.py

--- SRGAN-VGG54.py
+++ SRGAN-VGG54.py
@@ -1,2 +1,96 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+import numpy as np
+
+import keras
+import tensorflow as tf 
+from keras.layers import Conv2D, Dense
+from keras.layers import concatenate, add
+from keras.layers import BatchNormalization, Activation, PReLU, LeakyReLU
+from keras.layers import Input, Flatten, UpSampling2D
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+import keras.backend as K
+from keras.applications.vgg19 import VGG19
+
+from time import time
+from shutil import rmtree
+
+from batch_generator import COCOBatchGenerator
+from bsd100_callback import BSD100_evaluate
+from utils import print_available_devices, deprocess_HR, deprocess_LR
+
+import matplotlib.pyplot as plt
+if tf.test.is_built_with_cuda():
+    data_format = 'channels_first'
+    keras.backend.set_image_data_format('channels_first')
+else:
+    data_format = 'channels_last'
+    keras.backend.set_image_data_format('channels_last')
+print("Keras : ", keras.__version__)
+print("\t data_format : ", keras.backend.image_data_format())
+print("Tensorflow : ", tf.__version__)
+print_available_devices()
+B = 16 # number of residual block
+
+batch_size = 16
+target_size = (96,96)
+downscale_factor = 4
+
+# axis used in Parametric ReLU !
+shared_axis = [1,2] if data_format == 'channels_last' else [2,3]
+
+# axis for Batch Normalization
+axis = -1 if data_format == 'channels_last' else 1
+# build a residual block
+def res_block(inputs):
+    x = Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation=None, use_bias=False)(inputs)
+    x = BatchNormalization(axis=axis)(x)
+    x = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=shared_axis)(x)
+    x = Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation=None, use_bias=False)(x)
+    x = BatchNormalization(axis=axis)(x)
+    return add([x, inputs])
+
+# build an upscale block
+# PixelShuffler is replaced by an UpSampling2D layer (nearest upsampling)
+
+def up_block(x):
+    x = Conv2D(256, kernel_size=(3,3), strides=(1,1) , padding='same', activation=None, use_bias=False)(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = PReLU(alpha_initializer='zeros', alpha_regularizer=None, alpha_constraint=None, shared_axes=shared_axis)(x)
+    return x
+input_generator = Input(shape=(None, None, 3) if data_format=='channels_last' else (3, None, None), 
+                        name='input_generator')
+
+x = Conv2D(filters=64, kernel_size=(9,9),
+           strides=(1,1), padding='same',
+           activation=None)(input_generator)
+
+x_input_res_block = PReLU(alpha_initializer='zeros',
+                          alpha_regularizer=None,
+                          alpha_constraint=None,
+                          shared_axes=shared_axis)(x)
+
+x = x_input_res_block
+
+# add B residual blocks 
+for i in range(B):
+    x = res_block(x)
+
+x = Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same', activation=None, use_bias=False)(x)
+x = BatchNormalization(axis=axis)(x)
+
+# skip connection
+x = add([x, x_input_res_block])
+
+# two upscale blocks
+x = up_block(x)
+x = up_block(x)
+
+# final conv layer : activated with tanh -> pixels in [-1, 1]
+output_generator = Conv2D(3, kernel_size=(9,9), 
+                          strides=(1,1), activation='tanh',
+                          use_bias=False, padding='same')(x)
+
+generator = Model(inputs=input_generator, outputs=output_generator)
